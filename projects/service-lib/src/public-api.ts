aca52960f7de8835ee0633feffba86811b0dbd95
sharath <sharaths.kashyap@gmail.com>
2019-07-10 12:15:11 +0530

    Issue #SB-13074 feat : Creation of Common Telemetry Service Implementation Across Portal & Mobile

--- projects/service-lib/src/public-api.ts
+++ projects/service-lib/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/service-lib.service';
 export * from './lib/service-lib.component';
 export * from './lib/service-lib.module';
+export * from './lib/telemetry/telemetry-generator.service';
